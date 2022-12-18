3b07fd79cfa4047b629bc750c6137a8bfb82e20b
Vlad <vladot@ya.ru>
2022-12-18 17:49:33 +0600

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
+
     public static void main(String[] args) {
-        
+        task1();
+//        task2();
+//        task3();
+    }
+
+
+    // Task 1
+    public static boolean isLeapYear(int year) {
+        boolean leapYear;
+        leapYear = (((year % 4) == 0) && ((year % 100) != 0)) || ((year % 400) == 0);
+        return leapYear;
+    }
+
+    public static void task1() {
+        System.out.println("Задача_3");
+        System.out.println();
+        int year = 2100;
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+
+        }
+
+        System.out.println();
+        System.out.println();
     }
 }
